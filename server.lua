845dc698215f10e190496d5467bcb7ba176e2ab0
RedAlex <unconfigured@null.spigotmc.org>
2018-07-17 09:23:54 -0400

    Rename ressource and event

--- server.lua
+++ server.lua
@@ -13,41 +13,41 @@ local LogConcess   = ""
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
 function loadLogs()
-  LogAmbulance = LoadResourceFile("NukeTown_logs", "Logs/ambulance.log") or ""
-  LogConcess   = LoadResourceFile("NukeTown_logs", "Logs/concess.log") or ""
-  LogMecano    = LoadResourceFile("NukeTown_logs", "Logs/mecano.log") or ""
-  LogPolice    = LoadResourceFile("NukeTown_logs", "Logs/police.log") or ""
-  LogSheriff   = LoadResourceFile("NukeTown_logs", "Logs/sheriff.log") or ""
-  LogTaxi      = LoadResourceFile("NukeTown_logs", "Logs/taxi.log") or ""
+  LogAmbulance = LoadResourceFile("esx_joblogs", "Logs/ambulance.log") or ""
+  LogConcess   = LoadResourceFile("esx_joblogs", "Logs/concess.log") or ""
+  LogMecano    = LoadResourceFile("esx_joblogs", "Logs/mecano.log") or ""
+  LogPolice    = LoadResourceFile("esx_joblogs", "Logs/police.log") or ""
+  LogSheriff   = LoadResourceFile("esx_joblogs", "Logs/sheriff.log") or ""
+  LogTaxi      = LoadResourceFile("esx_joblogs", "Logs/taxi.log") or ""
 end
 
 function SaveInLog(job, message)
     if job == "ambulance" then
 	    LogAmbulance = LogAmbulance .. message .. "\n"
-      SaveResourceFile("NukeTown_logs", "Logs/ambulance.log", LogAmbulance, -1)
+      SaveResourceFile("esx_joblogs", "Logs/ambulance.log", LogAmbulance, -1)
     elseif job == "concess" then
 	    LogConcess = LogConcess .. message .. "\n"
-      SaveResourceFile("NukeTown_logs", "Logs/concess.log", LogConcess, -1)
+      SaveResourceFile("esx_joblogs", "Logs/concess.log", LogConcess, -1)
     elseif job == "mecano" then
 	    LogMecano = LogMecano .. message .. "\n"
-      SaveResourceFile("NukeTown_logs", "Logs/mecano.log", LogMecano, -1)
+      SaveResourceFile("esx_joblogs", "Logs/mecano.log", LogMecano, -1)
     elseif job == "police" then
 	    LogPolice = LogPolice .. message .. "\n"
-      SaveResourceFile("NukeTown_logs", "Logs/police.log", LogPolice, -1)
+      SaveResourceFile("esx_joblogs", "Logs/police.log", LogPolice, -1)
     elseif job == "sheriff" then
 	    LogSheriff = LogSheriff .. message .. "\n"
-      SaveResourceFile("NukeTown_logs", "Logs/sheriff.log", LogSheriff, -1)
+      SaveResourceFile("esx_joblogs", "Logs/sheriff.log", LogSheriff, -1)
     elseif job == "taxi" then
 	    LogTaxi = LogTaxi .. message .. "\n"
-      SaveResourceFile("NukeTown_logs", "Logs/taxi.log", LogTaxi, -1)
+      SaveResourceFile("esx_joblogs", "Logs/taxi.log", LogTaxi, -1)
     else
       print("Une erreur c'est produite dans esx_joblogs")
     end
 end
 
 
-RegisterServerEvent('NukeTown_logs:AddInLog')
-AddEventHandler('NukeTown_logs:AddInLog', function(job, message)
+RegisterServerEvent('esx_joblogs:AddInLog')
+AddEventHandler('esx_joblogs:AddInLog', function(job, message)
   local _job = job
   local _message = message
   SaveInLog(_job, _message)
