c5a3062135ab2959541726b07b69fe90f0475ff4
RedAlex <redalex24@gmail.com>
2018-07-27 22:30:35 -0400

    setup AddInLog to 4 info recive
    & Fix Space

--- server.lua
+++ server.lua
@@ -24,35 +24,46 @@ end
 function SaveInLog(job, message)
     if job == "ambulance" then
 	    LogAmbulance = LogAmbulance .. message .. "\n"
-      SaveResourceFile("esx_joblogs", "Logs/ambulance.log", LogAmbulance, -1)
+        SaveResourceFile("esx_joblogs", "Logs/ambulance.log", LogAmbulance, -1)
     elseif job == "concess" then
 	    LogConcess = LogConcess .. message .. "\n"
-      SaveResourceFile("esx_joblogs", "Logs/concess.log", LogConcess, -1)
+        SaveResourceFile("esx_joblogs", "Logs/concess.log", LogConcess, -1)
     elseif job == "mecano" then
 	    LogMecano = LogMecano .. message .. "\n"
-      SaveResourceFile("esx_joblogs", "Logs/mecano.log", LogMecano, -1)
+        SaveResourceFile("esx_joblogs", "Logs/mecano.log", LogMecano, -1)
     elseif job == "police" then
 	    LogPolice = LogPolice .. message .. "\n"
-      SaveResourceFile("esx_joblogs", "Logs/police.log", LogPolice, -1)
+        SaveResourceFile("esx_joblogs", "Logs/police.log", LogPolice, -1)
     elseif job == "sheriff" then
 	    LogSheriff = LogSheriff .. message .. "\n"
-      SaveResourceFile("esx_joblogs", "Logs/sheriff.log", LogSheriff, -1)
+        SaveResourceFile("esx_joblogs", "Logs/sheriff.log", LogSheriff, -1)
     elseif job == "taxi" then
 	    LogTaxi = LogTaxi .. message .. "\n"
-      SaveResourceFile("esx_joblogs", "Logs/taxi.log", LogTaxi, -1)
+        SaveResourceFile("esx_joblogs", "Logs/taxi.log", LogTaxi, -1)
     else
-      print("Une erreur c'est produite dans esx_joblogs")
+        print("Une erreur c'est produite dans esx_joblogs")
     end
 end
 
 
 RegisterServerEvent('esx_joblogs:AddInLog')
-AddEventHandler('esx_joblogs:AddInLog', function(job, localetxt, sourcename, targetname)
+AddEventHandler('esx_joblogs:AddInLog', function(job, localetxt, info1, info2, info3, info4)
   local _job        = job
   local _localetxt  = localetxt
-  local _sourcename = sourcename
-  local _targetname = targetname
-  local message     = "["..os.date("%c").."] ' ".. _U(_localetxt, _sourcename, _targetname)
+  local _info1      = info1
+  local _info2      = ''
+  local _info3      = ''
+  local _info4      = ''
+  if info2 ~= nil then
+    _info2 = info2
+  end
+  if info3 ~= nil then
+    _info3 = info3
+  end
+  if info4 ~= nil then
+    _info4 = info4
+  end
+  local message     = "["..os.date("%c").."] ' ".. _U(_localetxt, _info1, _info2, _info3, _info4)
   SaveInLog(_job, message)
 end)
 
