410d3b978c6c9118a91e9a938c95891355c9d1f2
MrFr4g <fragmatheo@gmail.com>
2018-07-31 01:54:33 +0200

    Add option discord link

--- server.lua
+++ server.lua
@@ -12,6 +12,24 @@ local LogConcess   = ""
 
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
+function sendToDiscord (canal, name, message, color)
+  -- Modify here your discordWebHook username = name, content = message,embeds = embeds
+local DiscordWebHook = canal
+local embeds = {
+    {
+        ["title"]= message,
+        ["type"]= "rich",
+        ["color"] = color,
+        ["footer"]=  {
+        ["text"]= "Job_logs",
+       },
+    }
+}
+
+  if message == nil or message == '' then return FALSE end
+  PerformHttpRequest(DiscordWebHook, function(err, text, headers) end, 'POST', json.encode({ username = name,embeds = embeds}), { ['Content-Type'] = 'application/json' })
+end
+
 function loadLogs()
   LogAmbulance = LoadResourceFile("esx_joblogs", "Logs/ambulance.log") or ""
   LogConcess   = LoadResourceFile("esx_joblogs", "Logs/concess.log")   or ""
@@ -25,21 +43,39 @@ function SaveInLog(job, message)
     if job == "ambulance" then
         LogAmbulance = LogAmbulance .. message .. "\n"
         SaveResourceFile("esx_joblogs", "Logs/ambulance.log", LogAmbulance, -1)
+          if Config.EnableDiscordLink == true then
+		        sendToDiscord(Config.webhookambulance, _U('ambulance_bot_name'), message, Config.orange)
+		      end
     elseif job == "concess" then
         LogConcess = LogConcess .. message .. "\n"
         SaveResourceFile("esx_joblogs", "Logs/concess.log", LogConcess, -1)
+          if Config.EnableDiscordLink == true then
+		        sendToDiscord(Config.webhookconcess, _U('concess_bot_name'), message, Config.orange)
+		      end
     elseif job == "mecano" then
         LogMecano = LogMecano .. message .. "\n"
         SaveResourceFile("esx_joblogs", "Logs/mecano.log", LogMecano, -1)
+          if Config.EnableDiscordLink == true then
+		        sendToDiscord(Config.webhookmecano, _U('mecano_bot_name'), message, Config.orange)
+		      end
     elseif job == "police" then
         LogPolice = LogPolice .. message .. "\n"
         SaveResourceFile("esx_joblogs", "Logs/police.log", LogPolice, -1)
+          if Config.EnableDiscordLink == true then
+		        sendToDiscord(Config.webhookpolice, _U('police_bot_name'), message, Config.orange)
+		      end
     elseif job == "sheriff" then
         LogSheriff = LogSheriff .. message .. "\n"
         SaveResourceFile("esx_joblogs", "Logs/sheriff.log", LogSheriff, -1)
+           if Config.EnableDiscordLink == true then
+		        sendToDiscord(Config.webhooksheriff, _U('sheriff_bot_name'), message, Config.orange)
+		       end
     elseif job == "taxi" then
         LogTaxi = LogTaxi .. message .. "\n"
         SaveResourceFile("esx_joblogs", "Logs/taxi.log", LogTaxi, -1)
+        if Config.EnableDiscordLink == true then
+		      sendToDiscord(Config.webhooktaxi, _U('taxi_bot_name'), message, Config.orange)
+		    end
     else
         print("Une erreur c'est produite dans esx_joblogs")
     end
